799544b12fe41e10f2a1205a3e25faf073bdd159
Colin McDonald <colinmcd0731@gmail.com>
2023-02-22 18:56:20 -0500

    Overhaul

--- cantor.hs
+++ cantor.hs
@@ -7,15 +7,49 @@ import Data.Maybe (fromJust)
 
 type Stream a = [a]
 
+newtype Bounds a = Bounds (Maybe Integer)
+unbounded = Bounds Nothing
+bounded = Bounds . Just
+
+joinBounds :: (Integer -> Integer -> Integer) -> Bounds a -> Bounds b -> Bounds c
+joinBounds f (Bounds a) (Bounds b) = Bounds (pure f <*> a <*> b)
+
+isBounded :: Bounds a -> Bool
+isBounded (Bounds Nothing) = False
+isBounded (Bounds (Just _)) = True
+
+drop' :: Integer -> [a] -> [a]
+drop' 0 as = as
+drop' n (_ : as) = drop' (pred n) as
+
+take' :: Integer -> [a] -> [a]
+take' 0 as = []
+take' n (a : as) = a : take' (pred n) as
+
+sublist :: Integer -> Integer -> [a] -> [a]
+sublist s e as = take' (e - s) (drop' s as)
+
 class Countable a where
   enumerate :: Stream a
+  toIndex :: a -> Integer
+  size :: Bounds a
 
---getIndex :: (Eq a, Countable a) => a -> Integer
-getIndex a = h 0 enumerate where
-  h i (a' : as)
-    | a == a' = i
-    | otherwise = h (succ i) as
-  h i [] = error "Error: element not in enumeration! You probably have a bad Countable instance definition"
+  {-# MINIMAL enumerate, toIndex, size #-}
+
+  enumerateFromTo :: a -> a -> Stream a
+  enumerateFrom :: a -> Stream a
+  enumerateTo :: a -> Stream a
+
+  enumerateFrom a = drop' (toIndex a - 1) enumerate
+  enumerateTo a = take' (toIndex a) enumerate
+  enumerateFromTo a b = sublist (toIndex a) (toIndex b + 1) enumerate
+
+-- getIndex :: (Eq a, Countable a) => a -> Integer
+-- getIndex a = h 0 enumerate where
+--   h i (a' : as)
+--    | a == a' = i
+--    | otherwise = h (succ i) as
+--   h i [] = error "Error: element not in enumeration! You probably have a bad Countable instance definition"
 
 diagonalize :: (Stream a, Stream b) -> Stream (a, b)
 diagonalize (as, bs) = h as bs [] [] where
@@ -29,8 +63,14 @@ diagonalize (as, bs) = h as bs [] [] where
   h [] (b : bs) pas pbs =
     zip pas (tail pbs ++ [b]) ++ h [] bs pas (tail pbs ++ [b])
 
+--assumes a and b are unbounded types
+undiagonalize :: Integer -> Integer -> Integer
+undiagonalize i j = j + (((i + j) * (i + j + 1)) `div` 2)
+
 --Powerset of a potentially infinite-length list
---powerset :: Stream a -> Stream [a]
+--N.B. this only produces finite-size members
+--(e.g. Evens \notin powerset(Nats))
+powerset :: Stream a -> Stream [a]
 powerset xs =
   [] : foldr
     (\ x next acc ->
@@ -38,7 +78,8 @@ powerset xs =
          acc' ++ (next (acc ++ acc')))
     (\ _ -> []) xs [[]]
 
---permutations :: Stream a -> Stream [a]
+-- No repetitions (set)
+permutations :: Stream a -> Stream [a]
 permutations xs =
   [] : foldr
     (\ x next acc ->
@@ -46,67 +87,169 @@ permutations xs =
          acc' ++ (next (acc ++ acc')))
     (\ _ -> []) xs [[]]
 
+-- With repetitions (array)
+permutationsWithRepetitions :: Stream a -> Stream [a]
+permutationsWithRepetitions xs =
+  [] : map (\(y, ys) -> y : ys) (diagonalize (xs, permutationsWithRepetitions xs))
+
+unpermuteWithRepetitions :: Countable a => [a] -> Integer
+unpermuteWithRepetitions [] = 0
+unpermuteWithRepetitions (a : as) = undiagonalize (toIndex a) (unpermuteWithRepetitions as)
+
+hotel :: Stream (Stream a) -> Stream [a]
+hotel [] = []
+hotel (as : bs) = map (\(a, b) -> a : b) (diagonalize (as, hotel bs))
+
+-- Yields all possible lists with elements of a stream
+--star :: Stream a -> Stream [a]
+--star as = map (\(n, b) -> ) (diagonalize ([0..], as))
+
+--unpermute :: Countable a => [a] 
+
 --Returns a list (of lists) of all the places we can insert an element into a list
---insertions :: a -> [a] -> [[a]]
+insertions :: a -> [a] -> [[a]]
 insertions a as = insertionsh a as [] where
   insertionsh :: a -> [a] -> [a] -> [[a]]
   insertionsh a [] bs' = [reverse (a : bs')]
   insertionsh a (b : bs) bs' =
     (reverse bs' ++ a : b : bs) : insertionsh a bs (b : bs')
 
---alternate :: [a] -> [a] -> [a]
+alternate :: [a] -> [a] -> [a]
 alternate (a : as) bs = a : alternate bs as
-alternate [] bs = bs
+--alternate [] bs = bs
 
---eithernate :: Stream a -> Stream b -> Stream (Either a b)
+eithernate :: Stream a -> Stream b -> Stream (Either a b)
 eithernate as bs = alternate (fmap Left as) (fmap Right bs)
 
 deriveBoundedEnumerate :: (Bounded a, Enum a) => [a]
 deriveBoundedEnumerate = [minBound..maxBound]
 
-zplus :: (Enum n, Num n) => [n]
-zplus  = let h n = n : h (succ n) in h (succ 0)
-zminus :: (Enum n, Num n) => [n]
-zminus = let h n = n : h (pred n) in h (pred 0)
+deriveBoundedIndex :: Enum a => a -> Integer
+deriveBoundedIndex = toInteger . fromEnum
 
+indexAsBound :: Enum a => a -> Bounds a
+indexAsBound = bounded . succ . toInteger . fromEnum
+
+deriveBoundedSize :: (Bounded a, Enum a) => Bounds a
+deriveBoundedSize = indexAsBound maxBound -- - fromEnum minBound
+
+zplus :: (Enum n, Num n) => [n]
+zplus = [1, 2..] -- strictly positive integers
+zminus :: (Enum n, Num n) => [n]
+zminus = [-1, -2..] -- strictly negative integers
+
+--instance (Enum a, Bounded a) => Countable a where
+--  enumerate = deriveBoundedEnumerate
+--  toIndex = fromEnum
 instance Countable Bool where
   enumerate = deriveBoundedEnumerate
+  toIndex = deriveBoundedIndex
+  size = deriveBoundedSize
+
 instance Countable () where
   enumerate = deriveBoundedEnumerate
+  toIndex = deriveBoundedIndex
+  size = deriveBoundedSize
+
 instance Countable Char where
   enumerate = deriveBoundedEnumerate
+  toIndex = deriveBoundedIndex
+  size = deriveBoundedSize
+
 instance Countable Word where
   enumerate = deriveBoundedEnumerate
+  toIndex = deriveBoundedIndex
+  size = deriveBoundedSize
+
 instance Countable Ordering where
   enumerate = deriveBoundedEnumerate
+  toIndex = deriveBoundedIndex
+  size = deriveBoundedSize
+
 instance Countable Int where
   enumerate = 0 : alternate zplus zminus
+  toIndex i
+    | i <  0 = -2 * toInteger i
+    | i == 0 = 0
+    | i >  0 = 2 * toInteger i - 1
+  size = bounded (1 + toInteger (maxBound :: Int) - toInteger (minBound :: Int))
+
 instance Countable Integer where
   enumerate = 0 : alternate zplus zminus
+  toIndex i
+    | i <  0 = -2 * i
+    | i == 0 = 0
+    | i >  0 = 2 * i - 1
+  size = unbounded
+
 instance Countable a => Countable [a] where
-  enumerate = permutations enumerate
+  enumerate = permutationsWithRepetitions enumerate
+  toIndex = unpermuteWithRepetitions
+  size = unbounded
+
 instance (Countable a, Countable b) => Countable (Either a b) where
-  enumerate = eithernate enumerate enumerate
+  enumerate = ((\a b -> case (isBounded a, isBounded b) of
+                   (True, True)   -> map Left  enumerate ++ map Right enumerate
+                   (True, False)  -> map Left  enumerate ++ map Right enumerate
+                   (False, True)  -> map Right enumerate ++ map Left  enumerate
+                   (False, False) -> alternate (map Left enumerate) (map Right enumerate))
+               :: (Countable a, Countable b) => Bounds a -> Bounds b -> Stream (Either a b))
+              size size
+  toIndex = either (\a -> 2 * toIndex a) (\b -> 1 + 2 * toIndex b)
+  size = (joinBounds (+) :: Bounds a -> Bounds b -> Bounds (Either a b)) size size
+
 instance Countable a => Countable (Maybe a) where
   enumerate = Nothing : fmap Just enumerate
-instance (Countable a, Countable b) => Countable (a, b) where
-  enumerate = diagonalize (enumerate, enumerate)
-instance (Countable a, Countable b, Countable c) => Countable (a, b, c) where
-  enumerate = fmap (\ ((a, b), c) -> (a, b, c)) (diagonalize (diagonalize (enumerate, enumerate), enumerate))
-instance (Countable a, Countable b, Countable c, Countable d) => Countable (a, b, c, d) where
-  enumerate = fmap (\ (((a, b), c), d) -> (a, b, c, d)) (diagonalize (diagonalize (diagonalize (enumerate, enumerate), enumerate), enumerate))
-instance (Countable a, Countable b, Countable c, Countable d, Countable e) => Countable (a, b, c, d, e) where
-  enumerate = fmap (\ ((((a, b), c), d), e) -> (a, b, c, d, e)) (diagonalize (diagonalize (diagonalize (diagonalize (enumerate, enumerate), enumerate), enumerate), enumerate))
-instance (Countable a, Countable b, Countable c, Countable d, Countable e, Countable f) => Countable (a, b, c, d, e, f) where
-  enumerate = fmap (\ (((((a, b), c), d), e), f) -> (a, b, c, d, e, f)) (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (enumerate, enumerate), enumerate), enumerate), enumerate), enumerate))
-instance (Countable a, Countable b, Countable c, Countable d, Countable e, Countable f, Countable g) => Countable (a, b, c, d, e, f, g) where
-  enumerate = fmap (\ ((((((a, b), c), d), e), f), g) -> (a, b, c, d, e, f, g)) (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (enumerate, enumerate), enumerate), enumerate), enumerate), enumerate), enumerate))
-instance (Countable a, Countable b, Countable c, Countable d, Countable e, Countable f, Countable g, Countable h) => Countable (a, b, c, d, e, f, g, h) where
-  enumerate = fmap (\ (((((((a, b), c), d), e), f), g), h) -> (a, b, c, d, e, f, g, h)) (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (enumerate, enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate))
--- ...
-instance (Countable a, Countable b, Countable c, Countable d, Countable e, Countable f, Countable g, Countable h, Countable i, Countable j, Countable k, Countable l, Countable m, Countable n, Countable o) => Countable (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o) where
-  enumerate = fmap (\ ((((((((((((((a, b), c), d), e), f), g), h), i), j), k), l), m), n), o) -> (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o))
-    (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (diagonalize (enumerate, enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate), enumerate))
+  toIndex = maybe 0 (\a -> 1 + toIndex a)
+  size = ((\(Bounds m) -> Bounds m) :: Bounds a -> Bounds (Maybe a)) size
 
---instance Countable (GHC.Real.Ratio Integer) where
---  enumerate = fmap (uncurry (GHC.Real.:%)) enumerate
+instance (Countable a, Countable b) => Countable (a, b) where
+  enumerate = ((\a b -> case (isBounded a, isBounded b) of
+                   (True, True)   -> [(a, b) | a <- enumerate, b <- enumerate]
+                   (True, False)  -> [(a, b) | b <- enumerate, a <- enumerate]
+                   (False, True)  -> [(a, b) | a <- enumerate, b <- enumerate]
+                   (False, False) -> diagonalize (enumerate, enumerate))
+               :: (Countable a, Countable b) => Bounds a -> Bounds b -> Stream (a, b))
+              size size
+  toIndex ab =
+      ((\(Bounds ar) (Bounds br) (a, b) i j ->
+          case (ar, br) of
+            (Just as, Just bs) -> i * bs + j
+            (Just as, Nothing) -> j * as + i
+            (Nothing, Just bs) -> i * bs + j
+            (Nothing, Nothing) -> undiagonalize i j)
+        :: (Countable a, Countable b) => Bounds a -> Bounds b -> (a, b) -> Integer -> Integer -> Integer)
+      size size ab (toIndex (fst ab)) (toIndex (snd ab))
+  size = (joinBounds (*) :: Bounds a -> Bounds b -> Bounds (a, b)) size size
+
+instance (Countable a, Countable b, Countable c) => Countable (a, b, c) where
+  enumerate = fmap (\ ((a, b), c) -> (a, b, c)) enumerate
+  toIndex (a, b, c) = toIndex ((a, b), c)
+  size = (joinBounds (*) :: Bounds (a, b) -> Bounds c -> Bounds (a, b, c)) size size
+
+instance (Countable a, Countable b, Countable c, Countable d) => Countable (a, b, c, d) where
+  enumerate = fmap (\ ((a, b, c), d) -> (a, b, c, d)) enumerate
+  toIndex (a, b, c, d) = toIndex ((a, b, c), d)
+  size = (joinBounds (*) :: Bounds (a, b, c) -> Bounds d -> Bounds (a, b, c, d)) size size
+
+instance (Countable a, Countable b, Countable c, Countable d, Countable e) => Countable (a, b, c, d, e) where
+  enumerate = fmap (\ ((a, b, c, d), e) -> (a, b, c, d, e)) enumerate
+  toIndex (a, b, c, d, e) = toIndex ((a, b, c, d), e)
+  size = (joinBounds (*) :: Bounds (a, b, c, d) -> Bounds e -> Bounds (a, b, c, d, e)) size size
+
+instance (Countable a, Countable b, Countable c, Countable d, Countable e, Countable f) => Countable (a, b, c, d, e, f) where
+  enumerate = fmap (\ ((a, b, c, d, e), f) -> (a, b, c, d, e, f)) enumerate
+  toIndex (a, b, c, d, e, f) = toIndex ((a, b, c, d, e), f)
+  size = (joinBounds (*) :: Bounds (a, b, c, d, e) -> Bounds f -> Bounds (a, b, c, d, e, f)) size size
+
+instance (Countable a, Countable b, Countable c, Countable d, Countable e, Countable f, Countable g) => Countable (a, b, c, d, e, f, g) where
+  enumerate = fmap (\ ((a, b, c, d, e, f), g) -> (a, b, c, d, e, f, g)) enumerate
+  toIndex (a, b, c, d, e, f, g) = toIndex ((a, b, c, d, e, f), g)
+  size = (joinBounds (*) :: Bounds (a, b, c, d, e, f) -> Bounds g -> Bounds (a, b, c, d, e, f, g)) size size
+
+instance (Countable a, Countable b, Countable c, Countable d, Countable e, Countable f, Countable g, Countable h) => Countable (a, b, c, d, e, f, g, h) where
+  enumerate = fmap (\ ((a, b, c, d, e, f, g), h) -> (a, b, c, d, e, f, g, h)) enumerate
+  toIndex (a, b, c, d, e, f, g, h) = toIndex ((a, b, c, d, e, f, g), h)
+  size = (joinBounds (*) :: Bounds (a, b, c, d, e, f, g) -> Bounds h -> Bounds (a, b, c, d, e, f, g, h)) size size
+
+-- ...
--- main.hs
+++ main.hs
@@ -2,4 +2,5 @@ module Main where
 import Cantor
 
 main =
-  putStrLn (show ((enumerate :: [(Int, Bool, Int, Char)]) !! 100000001))
+  putStrLn (show ((enumerate :: [(Integer, Bool, Integer, Char)]) !! 1000001))
+--  >> putStrLn (show (toIndex (0 :: Integer, True, 13 :: Integer, '\1010')))
